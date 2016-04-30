1e17cf534b0e421bf8ea2deccac734a448cb8bcf
Alex Atmore <acatmore@gmail.com>
2016-04-30 15:48:11 -0500

    new .old directory for the new install

--- civility.old/lib/collections.js
+++
@@ -1,4 +0,0 @@
-ResidentList = new Mongo.Collection('residentList');
-Homes = new Mongo.Collection("homes");
-Finances = new Mongo.Collection("finances");
-
--- civility.old/server/publications.js
+++
@@ -1,3 +0,0 @@
-Meteor.publish('getResidentsByIds', function(residentsArray) {
-  return Meteor.users.find({$in: residentsArray})
-})
