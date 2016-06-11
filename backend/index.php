db46f19cb2eeed82768badcfd308695a35d71650
Alex Atmore <acatmore@gmail.com>
2016-06-11 14:09:07 -0700

    started homes database in php

---
+++ backend/index.php
@@ -0,0 +1,16 @@
+<html>
+<?php
+//database info, change for your setup
+$dbhost = "127.0.0.1";
+$dbuser = "myuser";
+$dbpass = "xxxx";
+$dbname = "civility";
+
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+?>
+<body>
+	<?php
+	?>
+</body>
+</html>
